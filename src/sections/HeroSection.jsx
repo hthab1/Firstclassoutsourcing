46dfaad51b7b78448bdbc198f8446d4f1c6b902e
hthab <habteweldht@gmail.com>
2023-01-04 13:53:41 +0300

    raleway font added

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -3,9 +3,11 @@ import CustomSection from "../components/CustomSection";
 
 function HeroSection() {
   return (
-    <CustomSection classNameParent={`bg-secondary`} classNameChild={``} >
-        <div className="absolute w-full h-full flex flex-row justify-end"></div>
-        <div className="text-white" >hello</div>
+    <CustomSection classNameParent={`bg-secondary`} classNameChild={``}>
+      <div className="absolute w-full h-full flex flex-row justify-end"></div>
+      <div className="text-white font-black fontRaleway text-8xl">
+        First Class Outsourcing service for your bussiness
+      </div>
     </CustomSection>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    colors:{
-      primary:"#219AD3",
-      secondary:"#081A4B",
-      teritiary:"#F4F4F9",
-      quaternary:"#D9D9D9",
-    }
+    colors: {
+      primary: "#219AD3",
+      secondary: "#081A4B",
+      teritiary: "#F4F4F9",
+      quaternary: "#D9D9D9",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
   },
   plugins: [],
-}
+};
